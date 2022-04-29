742cd7aa2371d106be91c43a5894871efb7bdd8b
Tamirlan <tuarshintamir@gmail.com>
2022-04-29 15:15:48 +0500

    Complete Mission_2

---
+++ Mission_002/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+void Random (int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+      mas[i] = new Random().Next(1,10);  
+    }
+}
+void Print (int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine(mas[i] + " ");
+    }
+    Console.WriteLine();
+}
+int Sum(int[] mas)
+{
+    int sum = 0;
+    for (int i = 0; i < mas.Length;)
+    {
+        sum = sum + mas[i];
+        i = i+2;
+    }
+    return sum;
+}
+int [] Arr = new int[7];
+Random(Arr);
+Print(Arr);
+Console.WriteLine($"Сумма нечетных элементов массива = {Sum(Arr)}");
